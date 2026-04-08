1dde00a10b494238a1710a18d4ec0e0327d19ad8
SupSripusitto <supsripusitto@gmail.com>
2026-04-09 05:30:15 +0700

    Revert "Add periodic BC, modifiable box size & average velocity"
    This reverts commit 3d5861ccd82c44eaa188862356c4f7e5e06c5371.

--- ideal-gas.py
+++ ideal-gas.py
@@ -1,27 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10 # Number of molecules
+n = 5 # Number of molecules
 dt = 0.01 # Step size
 
-xrange = 6 
-yrange = 7 # Define the box size
-
-v_rms = 10 # Average velocity
-
-v = (np.random.rand(2,n)-0.5)*v_rms
+v = np.random.rand(2,n)-0.5
 x = np.random.rand(2,n)
-x[0,:] = x[0,:]*xrange
-x[1,:] = x[1,:]*yrange
 
 for i in range(100):
     x += v*dt
-    x[0,:] = x[0,:] % xrange
-    x[1,:] = x[1,:] % yrange
     plt.clf()
-    plt.xlim(0,xrange)
-    plt.ylim(0,yrange)
+    plt.xlim(0,1)
+    plt.ylim(0,1)
     plt.scatter(x[0,:],x[1,:])
-    plt.pause(0.0001)
+    plt.pause(0.00001)
 
 plt.show()
